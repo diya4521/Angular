9283b6a05f2edfa75cf4877856e9d16edad51280
harshid9898 <harshidthakor385@gamil.com>
2025-07-23 15:12:08 +0530

    'addfile'

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './page/login/login.component';
 import { HeaderComponent } from './com/header/header.component';
 import { FileManagerComponent } from './files/file-manager/file-manager.component';
+import { SideComponent } from './com/side/side.component';
 
 export const routes: Routes = [
-  { path: 'file', component: FileManagerComponent },
   { path: '', component: LoginComponent },
+  { path: 'file', component: FileManagerComponent },
+  { path: 'side', component: SideComponent },
 ];
